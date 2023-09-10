a95b1763f757107853c00a78c3a41936aa40329d
EDWINWASARI <edwinwasari@gmail.com>
2023-09-09 22:11:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * A C program that contains print function
- * return:0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
