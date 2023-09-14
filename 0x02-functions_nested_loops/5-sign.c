8a2dae649457efd259f20850ab566a9f033036a8
EDWINWASARI <edwinwasari@gmail.com>
2023-09-14 02:20:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,22 +10,22 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+if (n > 0)
+{
+_putchar('+'); /* Print + for positive numbers */
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0'); /* Print 0 for zero */
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-'); /* Print - for negative numbers */
+return (-1);
+}
 
-    /* Handle all cases (positive, zero, negative) */
-    return (0);
+/* Handle all cases (positive, zero, negative) */
+return (0);
 }
