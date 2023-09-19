2e88deda0faba96fe0c8c774a2a9173f5762ee1d
EDWINWASARI <edwinwasari@gmail.com>
2023-09-19 11:55:12 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,13 @@ void rev_string(char *s)
 int len = strlen(s);
 int middle = len / 2;
 char temp;
-for (int i = 0; i < middle; i++)
+int i;
+i = 0;
+while (i < middle)
 {
 temp = s[i];
 s[i] = s[len - i - 1];
 s[len - i - 1] = temp;
+i++;
 }
 }
